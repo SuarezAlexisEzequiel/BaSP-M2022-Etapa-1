af869eeade38cda0f5addaf27f795ac98535ac7c
Alexis Suarez <alexsuarezproyecto@gmail.com>
2022-04-14 15:52:39 -0300

    BaSP-W05-2022: 01_varables_and_operators Exercise done

--- semana-05/exercises/01_variables_and_operators.js
+++ semana-05/exercises/01_variables_and_operators.js
@@ -1,1 +1,29 @@
-console.log('Testing JS script');
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
+
+/* Punto A - Crear 2 variables numericas y utilizar el operador suma para guardar el valor de
+la suma de ambos numeros en una 3er variable */
+
+console.log('-Exercise 1.a:');
+var numOne = 13;
+var numTwo = 23;
+var numThree = numOne + numTwo;
+console.log ('numOne (' + numOne + ') + ' + 'numTwo (' + numTwo + ') = numThree (' + numThree + ')');
+
+/* Punto B - Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable. */
+
+console.log('\n-Exercise 1.b:');
+var strOne = 'Alexis';
+var strTwo = 'Suarez';
+var strThree = strOne + strTwo;
+console.log ('strOne (' + strOne + ') ' + 'strTwo (' + strTwo + ') = strThree (' + strThree + ')');
+
+/* Punto C - Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable */
+
+console.log('\n-Exercise 1.c:');
+var strFour = 'Exercise';
+var strFive = 'Done';
+var strSix = strFour + strFive;
+console.log ('strFour = ' + strFour + '(length = ' + strFour.length + ')' );
+console.log ('strFive = ' + strFive + '(lenght = ' + strFive.length + ')' );
+console.log ('strSix = ' + strSix + '(lenght = ' + strSix.length + ')' );
+
