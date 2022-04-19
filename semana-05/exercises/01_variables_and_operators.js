17c8c2a0e3bc6c6d49e1a033bfed580b6d534a3d
Alexis Suarez <alexsuarezproyecto@gmail.com>
2022-04-19 17:52:27 -0300

    BaSP-W05-2022: 01_variables_and_operators Applied ortography corrections

--- semana-05/exercises/01_variables_and_operators.js
+++ semana-05/exercises/01_variables_and_operators.js
@@ -7,7 +7,7 @@ console.log('-Exercise 1.a:');
 var numOne = parseFloat(prompt('Write the 1st number'));
 var numTwo = parseFloat(prompt('Write the 2nd number'));
 var numThree = numOne + numTwo;
-console.log ('numOne (' + numOne + ') + ' + 'numTwo (' + numTwo + ') = numThree (' + numThree + ')');
+console.log (numOne + ' + ' + numTwo + ' = ' + numThree);
 
 /* Punto B - Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable */
 
@@ -24,6 +24,6 @@ console.log('\n-Exercise 1.c:');
 var strFour = prompt('Write the 1st word');
 var strFive = prompt('Write the 2nd word');
 var strSix = strFour + strFive;
-console.log ('strFour = ' + strFour + '(length = ' + strFour.length + ')' );
-console.log ('strFive = ' + strFive + '(lenght = ' + strFive.length + ')' );
-console.log ('strSix = ' + strSix + '(lenght = ' + strSix.length + ')' );
+console.log ('strFour = ' + strFour + '(length = ' + strFour.length + ')');
+console.log ('strFive = ' + strFive + '(lenght = ' + strFive.length + ')');
+console.log ('strSix = ' + strSix + '(lenght = ' + strSix.length + ')');
